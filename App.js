537882daeafa74f861a57da6e5e61a3d448024eb
anilyavas <anil.yavas99@gmail.com>
2024-02-26 15:47:57 +0300

    placing bird

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Canvas, Circle, Group, Image } from '@shopify/react-native-skia';
 import { useWindowDimensions } from 'react-native';
 import { useImage } from '@shopify/react-native-skia';
@@ -6,10 +5,19 @@ import { useImage } from '@shopify/react-native-skia';
 const App = () => {
   const { width, height } = useWindowDimensions();
   const bg = useImage(require('./assets/sprites/background-day.png'));
+  const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
   const r = width * 0.33;
   return (
     <Canvas style={{ width, height }}>
       <Image image={bg} width={width} height={height} fit={'cover'} />
+      <Image
+        image={bird}
+        x={width / 4}
+        y={height / 2 - 24}
+        width={64}
+        height={48}
+        fit={'contain'}
+      />
     </Canvas>
   );
 };
