68c714203586152e508c980c95bbe3186925c35b
anilyavas <anil.yavas99@gmail.com>
2024-02-27 12:06:09 +0300

    offset

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Canvas, Circle, Group, Image } from '@shopify/react-native-skia';
+import { Canvas, Image } from '@shopify/react-native-skia';
 import { useWindowDimensions } from 'react-native';
 import { useImage } from '@shopify/react-native-skia';
 
@@ -6,20 +6,28 @@ const App = () => {
   const { width, height } = useWindowDimensions();
   const bg = useImage(require('./assets/sprites/background-day.png'));
   const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
-  const pipe = useImage(require('./assets/sprites/pipe-green.png'));
+  const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'));
   const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'));
-  const r = width * 0.33;
+
+  const pipeOffset = 100;
+
   return (
     <Canvas style={{ width, height }}>
       <Image image={bg} width={width} height={height} fit={'cover'} />
       <Image
-        image={pipe}
-        y={height - 320}
+        image={pipeBottom}
+        y={height - 320 - pipeOffset}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+      <Image
+        image={pipeTop}
+        y={pipeOffset - 320}
         x={width / 2}
         width={104}
         height={640}
       />
-      <Image image={pipeTop} y={-320} x={width / 2} width={104} height={640} />
       <Image
         image={bird}
         x={width / 4}
